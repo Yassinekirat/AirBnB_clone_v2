78a507d1f50ec03ef90bfae97b3008da06811aaa
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-21 19:29:27 +0100

    task  Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisnotfun(text):
+    """Displays 'C' followed by <text>."""
+    return f"C {format(text.replace('_', ' '))}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py(text='is cool'):
+    """ display “Python ”, followed by the value of the text"""
+    return f"Python {format(text.replace('_', ' '))}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def num(n):
+    """display “n is a number” only if n is an integer"""
+    n = int(n)
+    if isinstance(n, int):
+        return f"n is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
