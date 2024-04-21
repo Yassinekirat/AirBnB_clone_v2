02ed4959290cff5d42d365a73345d73f39c27303
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-21 21:52:43 +0100

    i am tired brain not braining

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,24 +3,24 @@
 starts a Flask web application
 """
 
-from flask import Flask, render_template
+from flask import Flask
 from models import *
 from models import storage
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page with the states listed in alphabetical order"""
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """closes the storage"""
+    """closes the storage on teardown"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
