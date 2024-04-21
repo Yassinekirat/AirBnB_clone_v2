e6fce6857764b016bc08a36dee6f05afe65bebaa
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-21 17:38:27 +0100

    fixing pycodeststyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello_hbnb():
     """'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
