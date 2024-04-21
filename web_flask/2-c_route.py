05c1badf47aa019ffdcd148f45682fbb703b7cb1
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-21 17:47:58 +0100

    task 2 C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    return f"C {format(text.replace("_", " "))}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
